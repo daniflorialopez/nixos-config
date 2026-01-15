1c0598ecfe482b82e43bfcede90e3acca2f5d51a
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-01-15 03:26:25 +0100

    Update hardware configuration with correct config for danix-hp

--- hosts/danix-hp/hardware-configuration.nix
+++ hosts/danix-hp/hardware-configuration.nix
@@ -5,24 +5,46 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "vmd" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  disko.devices.disk.main = {
-    device = lib.mkForce "/dev/nvme0n1";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/d2306bda-3822-4312-a9bd-8ff2193415ed";
+      fsType = "btrfs";
+      options = [ "subvol=root" ];
+    };
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/d2306bda-3822-4312-a9bd-8ff2193415ed";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/disk/by-uuid/d2306bda-3822-4312-a9bd-8ff2193415ed";
+      fsType = "btrfs";
+      options = [ "subvol=persist" ];
+    };
+
+  fileSystems."/swap" =
+    { device = "/dev/disk/by-uuid/d2306bda-3822-4312-a9bd-8ff2193415ed";
+      fsType = "btrfs";
+      options = [ "subvol=swap" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/E315-0F30";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
