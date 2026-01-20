29faebdd46c81fa09b84682765b43c081273a19e
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-01-20 18:26:58 +0100

    Add warpd package for mouse manipulation on x or wayland

--- home/dani/programs/misc.nix
+++ home/dani/programs/misc.nix
@@ -5,6 +5,7 @@
     libreoffice
     pcmanfm # or another file manager like thunar, nautilus, nemo, dolphin...
     vesktop
+    warpd # modal keyboard driven interface for mouse manipulation, it works also on wayland
   ];
 }
 
