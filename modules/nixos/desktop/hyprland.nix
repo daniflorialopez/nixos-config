ecfae1b49247d8aab8e06de1b156b873bb9a171d
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-02-25 14:03:13 +0100

    Add hyprland basic configuration

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -3,15 +3,24 @@
 {
   programs.hyprland = {
     enable = true;
-    # withUWSM = true; # once you’re on a release that has it and want it
-    xwayland.enable = true;
+    withUWSM = true; # once on a release that has it and want it
+    xwayland.enable = true; # keep compatibility for X11-only apps
+  };
+
+  # Wayland portals (screen sharing, file pickers, etc)
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
   };
 
   environment.systemPackages = with pkgs; [
-    kitty  # or your preferred terminal, needed by default Hyprland config
+    alacritty # or the preferred terminal, needed by default Hyprland config
   ];
 
-  # Optional: make Electron apps use Wayland:
-  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # Electron apps use Wayland:
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
 
