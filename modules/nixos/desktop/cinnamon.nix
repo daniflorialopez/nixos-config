0d03460d7b1076851e17803138e1935de29a30f9
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-02-25 13:56:05 +0100

    Trim Cinnamon configuration to act only as fallback

--- modules/nixos/desktop/cinnamon.nix
+++ modules/nixos/desktop/cinnamon.nix
@@ -1,10 +1,6 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
-  services.xserver = {
-    enable = true;
-    desktopManager.cinnamon.enable = true;
-    displayManager.lightdm.enable = true; # or gdm, sddm, etc.
-  };
+  services.xserver.desktopManager.cinnamon.enable = true;
 }
 
