f205093cb3bc125d5b209f4bbfe25e8af4a3706a
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-02-28 15:49:33 +0100

    Add keybinds for workspaces and remove hyprland-managed services to manage them through systemd

--- home/dani/wm/hyprland.nix
+++ home/dani/wm/hyprland.nix
@@ -5,6 +5,10 @@
 }:
 
 {
+  imports = [
+    ./hyprland-services.nix
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
 
@@ -16,21 +20,40 @@
       "$mod" = "SUPER";
 
       exec-once = [
-        # Polkit prompts (wifi passwords, admin actions, etc.)
-        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-
-        "waybar"
-        "mako"
-        "nm-applet --indicator"
       ];
 
       bind = [
+        # Basic binds
         "$mod, Return, exec, alacritty"
         "$mod, D, exec, wofi --show drun"
         "$mod, Q, killactive"
         "$mod, M, exit"
         "$mod, F, fullscreen"
         "$mod, Space, togglefloating"
+
+        # Workspaces
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        # Move to workspaces
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
 
         # Screenshot area to clipboard
         ", Print, exec, grim -g \"$(slurp)\" - | wl-copy"
@@ -61,8 +84,8 @@
     # essentials
     alacritty
     wofi
-    waybar
-    mako
+    # waybar
+    # mako
     hyprpaper
 
     # clipboard + screenshots
