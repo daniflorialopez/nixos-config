0389cd8fb84c75cf33abb702f7a1fbe58322bf55
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-03-05 12:55:12 +0100

    Change Desktop Manager to SDDM and theme to Breeze

--- modules/nixos/desktop/sddm.nix
+++ modules/nixos/desktop/sddm.nix
@@ -1,9 +1,31 @@
-{ ... }:
+{
+  pkgs,
+  ...
+}:
 {
   services.xserver.enable = true; # needed for Cinnamon - X11 session
 
+  environment.systemPackages = with pkgs.kdePackages; [
+    plasma-desktop
+  ];
+
   services.displayManager.sddm = {
     enable = true;
+    package = pkgs.kdePackages.sddm;
+    theme = "breeze";
     wayland.enable = true; # wayland-capable greeter - Hyprland session
+
+    # enableHidpi = true # only if there is a HiDPI screen
+    
+    settings = {
+      Theme = {
+        CursorTheme = "breeze_cursors";
+        # CursorSize = 24; # optional
+      };
+    };
+
+    extraPackages = with pkgs.kdePackages; [
+      plasma-desktop
+    ];
   };
 }
