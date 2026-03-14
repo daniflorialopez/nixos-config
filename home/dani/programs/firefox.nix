80f6219a279ea8a8caf4a86b8ddf05fbd474607a
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-03-14 23:49:22 +0100

    Add themes to Firefox profiles

--- home/dani/programs/firefox.nix
+++ home/dani/programs/firefox.nix
@@ -8,14 +8,31 @@
       personal = {
         id = 0;
         isDefault = true;
+
+        settings = {
+          "extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
+          "services.sync.prefs.sync.extensions.activeThemeID" = false;
+        };
       };
 
       work = {
         id = 1;
+
+        settings = {
+          # Optional:
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org"; # uncomment this to pin built-in Dark declaratively.
+          "services.sync.prefs.sync.extensions.activeThemeID" = false;
+        };
       };
 
       lab = {
         id = 2;
+
+        settings = {
+          "extensions.activeThemeID" = "{d551e720-3100-43f5-8242-74a6c5cb7c34}";
+          "services.sync.prefs.sync.extensions.activeThemeID" = false;
+        };
+
       };
     };
   };
