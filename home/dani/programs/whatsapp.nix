754902d139ff95f1f9ca8a0d2e1c8aba809d1b3d
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-03-30 11:25:27 +0200

    Add whatsapp webapp

---
+++ home/dani/programs/whatsapp.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+let
+  profileDir = "${config.home.homeDirectory}/.local/share/chromium-whatsapp";
+
+  whatsapp-web = pkgs.writeShellApplication {
+    name = "whatsapp-web";
+    runtimeInputs = [ pkgs.chromium ];
+    text = ''
+      exec ${pkgs.chromium}/bin/chromium \
+        --ozone-platform-hint=auto \
+        --enable-wayland-ime \
+        --app=https://web.whatsapp.com \
+        --user-data-dir="${profileDir}" \
+        "$@"
+    '';
+  };
+in
+{
+  home.packages = [
+    whatsapp-web
+  ];
+
+  xdg.desktopEntries.whatsapp-web = {
+    name = "WhatsApp Web";
+    exec = "${whatsapp-web}/bin/whatsapp-web";
+    terminal = false;
+    categories = [ "Network" "InstantMessaging" ];
+    startupNotify = true;
+  };
+
+  wayland.windowManager.hyprland.settings.bind = [
+    "$mod SHIFT, W, exec, ${whatsapp-web}/bin/whatsapp-web"
+  ];
+}
