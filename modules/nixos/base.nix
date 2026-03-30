1f0dc0162abb253e6472aeaa5a89939a5ca46102
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-03-30 17:50:56 +0200

    Configure nh automatic cleanup and generation retention

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -56,16 +56,20 @@
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Automatically garbage-collect
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
+  programs.nh = {
+    enable = true;
+    clean = {
+      enable = true;
+      dates = "weekly";
+      extraArgs = "--keep-since 14d --keep 10";
+    };
   };
 
   # Deduplicate/optimise store a bit over time
-  nix.optimise.automatic = true;
-  nix.optimise.dates = [ "weekly" ];
+  nix.optimise = {
+    automatic = true;
+    dates = [ "weekly" ];
+  };
 
   # Limit how many boot entries are kept (doesn't free space by itself, but keeps boot menu tidy)
   boot.loader.systemd-boot.configurationLimit = 10;
