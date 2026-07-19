3e7b5dd7a5bd57638b57e7b6cf45e70cf3ca80e6
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-07-19 14:29:02 +0200

    Collapse the three copy-pasted hosts into a mkHost helper

--- flake.nix
+++ flake.nix
@@ -50,112 +50,54 @@
     }:
     let
       system = "x86_64-linux";
+
+      # Every host is wired identically: hosts/<name> plus the shared
+      # agenix + Home Manager plumbing. Keeping this in one place means
+      # the VM really does mirror the real machines (the LUKS rehearsal
+      # in danixos-vm depends on that symmetry).
+      mkHost =
+        name:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          # Extra args you want modules to see (if needed later)
+          specialArgs = { inherit inputs; };
+
+          modules = [
+            # Host config (imports hardware + modules/nixos/*.nix)
+            ./hosts/${name}
+
+            # Secrets management
+            inputs.agenix.nixosModules.default
+
+            # Home Manager as a NixOS module
+            home-manager.nixosModules.home-manager
+
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.sharedModules = [
+                nix-index-database.homeModules.default
+              ];
+
+              # Tell HM what extension to use when backing up conflicting files
+              home-manager.backupFileExtension = "hm-backup";
+
+              # You can pass extra args to home modules if you want
+              home-manager.extraSpecialArgs = { inherit inputs; };
+
+              # Your home config entry point
+              home-manager.users.dani = import ./home/dani;
+            }
+          ];
+        };
     in
     {
-      nixosConfigurations.danixos-vm = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        # Extra args you want modules to see (if needed later)
-        specialArgs = { inherit inputs; };
-
-        modules = [
-          # Host config (imports hardware + modules/nixos/*.nix)
-          ./hosts/danixos-vm
-
-          # Secrets management
-          inputs.agenix.nixosModules.default
-
-          # Home Manager as a NixOS module
-          home-manager.nixosModules.home-manager
-
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.sharedModules = [
-              nix-index-database.homeModules.default
-            ];
-
-            # Tell HM what extension to use when backing up conflicting files
-            home-manager.backupFileExtension = "hm-backup";
-
-            # You can pass extra args to home modules if you want
-            home-manager.extraSpecialArgs = { inherit inputs; };
-
-            # Your home config entry point
-            home-manager.users.dani = import ./home/dani;
-          }
-        ];
-      };
-      nixosConfigurations.danix-hp = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        # Extra args you want modules to see (if needed later)
-        specialArgs = { inherit inputs; };
-
-        modules = [
-          # Host config (imports hardware + modules/nixos/*.nix)
-          ./hosts/danix-hp
-
-          # Secrets management
-          inputs.agenix.nixosModules.default
-
-          # Home Manager as a NixOS module
-          home-manager.nixosModules.home-manager
-
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.sharedModules = [
-              nix-index-database.homeModules.default
-            ]; 
-
-            # Tell HM what extension to use when backing up conflicting files
-            home-manager.backupFileExtension = "hm-backup";
-
-            # You can pass extra args to home modules if you want
-            home-manager.extraSpecialArgs = { inherit inputs; };
-
-            # Your home config entry point
-            home-manager.users.dani = import ./home/dani;
-          }
-        ];
-      };
-      nixosConfigurations.legionix = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        # Extra args you want modules to see (if needed later)
-        specialArgs = { inherit inputs; };
-
-        modules = [
-          # Host config (imports hardware + modules/nixos/*.nix)
-          ./hosts/legionix
-
-          # Secrets management
-          inputs.agenix.nixosModules.default
-
-          # Home Manager as a NixOS module
-          home-manager.nixosModules.home-manager
-
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.sharedModules = [
-              nix-index-database.homeModules.default
-            ]; 
-
-            # Tell HM what extension to use when backing up conflicting files
-            home-manager.backupFileExtension = "hm-backup";
-
-            # You can pass extra args to home modules if you want
-            home-manager.extraSpecialArgs = { inherit inputs; };
-
-            # Your home config entry point
-            home-manager.users.dani = import ./home/dani;
-          }
-        ];
+      nixosConfigurations = {
+        danixos-vm = mkHost "danixos-vm";
+        danix-hp = mkHost "danix-hp";
+        legionix = mkHost "legionix";
       };
     };
 }
