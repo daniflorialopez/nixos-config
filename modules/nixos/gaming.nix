e7a3dd2780f97c440f9ed2342b9f607ccac8a472
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-07-19 02:22:49 +0200

    Add GE-Proton and gamemode for smoother gaming

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
 {
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    # GE-Proton as a selectable compat tool: eFootball freezes at launch
+    # on newer stock Proton (server handshake), GE gets better reports
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+  };
+
+  # CPU performance governor while a game runs; opt in per game with
+  # the "gamemoderun %command%" launch option in Steam
+  programs.gamemode.enable = true;
 
   environment.systemPackages = with pkgs; [
 
--- modules/nixos/users/dani.nix
+++ modules/nixos/users/dani.nix
@@ -6,7 +6,8 @@
 
   users.users.dani = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" ];
+    # gamemode: renice privileges only apply to group members
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "gamemode" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGSZH6iO1s9ne9mPCOOPrBdNftkJVNMFdymjXeOiufe dani@danarchy"
     ]; 
