d308da85d179e5303388b1d82cd7b8e7ee1afb5a
Daniel Floria Lopez <daniflorialopez@gmail.com>
2026-07-20 11:16:21 +0200

    Authorize legionix's own SSH key for dani

--- modules/nixos/users/dani.nix
+++ modules/nixos/users/dani.nix
@@ -10,7 +10,8 @@
     extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "gamemode" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGSZH6iO1s9ne9mPCOOPrBdNftkJVNMFdymjXeOiufe dani@danarchy"
-    ]; 
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICs56p3R01JJSho9vxH2sr8vqZjjVXqCmgwpNsHpE4N6 dani@legionix"
+    ];
     shell = pkgs.fish;
   };
 }
